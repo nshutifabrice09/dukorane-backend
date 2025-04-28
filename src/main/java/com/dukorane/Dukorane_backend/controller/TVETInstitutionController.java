5e58519222b782d793a2d395990300f8b018219a
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-04-28 10:28:53 +0200

    final commit

--- src/main/java/com/dukorane/Dukorane_backend/controller/TVETInstitutionController.java
+++ src/main/java/com/dukorane/Dukorane_backend/controller/TVETInstitutionController.java
@@ -4,10 +4,10 @@ package com.dukorane.Dukorane_backend.controller;
 import com.dukorane.Dukorane_backend.model.TVETInstitution;
 import com.dukorane.Dukorane_backend.service.TVETInstitutionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
@@ -24,4 +24,25 @@ public class TVETInstitutionController {
     public TVETInstitution saveTVETInstitution(@RequestBody TVETInstitution tvetInstitution){
         return tvetInstitutionService.saveTVETInstitution(tvetInstitution);
     }
+
+    @GetMapping("/tvetInstitutions")
+    public List<TVETInstitution> tvetInstitutionList(){
+        return tvetInstitutionService.getAllTVETInstitutions();
+    }
+
+    @GetMapping("/tvetInstitution/{id}")
+    public TVETInstitution findById(@PathVariable ("id")UUID id){
+        return tvetInstitutionService.getTVETInstitutionById(id);
+    }
+
+    @PutMapping("/update/tvetInstitution/{id}")
+    public TVETInstitution updateTVETInstitution(@PathVariable ("id") UUID id, @RequestBody TVETInstitution tvetInstitution){
+        return tvetInstitutionService.updateTVETInstitution(id, tvetInstitution);
+    }
+
+    @DeleteMapping("/delete/tvetInstitution/{id}")
+    public void deleteById(@PathVariable ("id") UUID id){
+        tvetInstitutionService.deleteById(id);
+    }
+
 }
