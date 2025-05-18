252a668b8f13d55ab6f7f68e1da2cec9b5c7900b
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-18 12:44:51 +0200

    service-impl-commit

--- src/main/java/com/dukorane/Dukorane_backend/service/UserServiceImplementation.java
+++ src/main/java/com/dukorane/Dukorane_backend/service/UserServiceImplementation.java
@@ -1,7 +1,34 @@
 package com.dukorane.Dukorane_backend.service;
 
+import com.dukorane.Dukorane_backend.model.User;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserServiceImplementation implements UserService{
+    @Override
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return null;
+    }
+
+    @Override
+    public User saveUser(User user) {
+        return null;
+    }
+
+    @Override
+    public User updateUser(Long id, User user) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
 }
