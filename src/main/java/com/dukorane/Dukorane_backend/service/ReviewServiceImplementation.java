3561d8ff9d1d6d67a429b2fa520c04e7865b62f4
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-18 12:41:51 +0200

    initial-service-impl-commit

--- src/main/java/com/dukorane/Dukorane_backend/service/ReviewServiceImplementation.java
+++ src/main/java/com/dukorane/Dukorane_backend/service/ReviewServiceImplementation.java
@@ -1,2 +1,7 @@
-package com.dukorane.Dukorane_backend.service;public class ReviewServiceImplementation {
+package com.dukorane.Dukorane_backend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewServiceImplementation implements ReviewService{
 }
