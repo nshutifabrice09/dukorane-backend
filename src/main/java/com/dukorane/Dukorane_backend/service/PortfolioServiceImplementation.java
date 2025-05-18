46344ffd258c12875460ee05cbc4375d4a3c7808
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-18 12:41:37 +0200

    initial-service-impl-commit

--- src/main/java/com/dukorane/Dukorane_backend/service/PortfolioServiceImplementation.java
+++ src/main/java/com/dukorane/Dukorane_backend/service/PortfolioServiceImplementation.java
@@ -1,2 +1,35 @@
-package com.dukorane.Dukorane_backend.service;public class PortfolioServiceImplementation {
+package com.dukorane.Dukorane_backend.service;
+
+import com.dukorane.Dukorane_backend.model.Portfolio;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PortfolioServiceImplementation implements PortfolioService{
+
+    @Override
+    public List<Portfolio> getAllPortfolios() {
+        return null;
+    }
+
+    @Override
+    public Portfolio getPortfolioById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Portfolio savePortfolio(Portfolio portfolio) {
+        return null;
+    }
+
+    @Override
+    public Portfolio updatePortfolio(Long id, Portfolio portfolio) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
 }
