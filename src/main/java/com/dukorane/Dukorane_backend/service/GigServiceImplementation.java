0c747452cb86bf4c552b95d4438ccd9e0678203b
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-18 12:40:27 +0200

    initial-service-impl-commit

--- src/main/java/com/dukorane/Dukorane_backend/service/GigServiceImplementation.java
+++ src/main/java/com/dukorane/Dukorane_backend/service/GigServiceImplementation.java
@@ -1,2 +1,34 @@
-package com.dukorane.Dukorane_backend.service;public class GigServiceImplementation {
+package com.dukorane.Dukorane_backend.service;
+
+import com.dukorane.Dukorane_backend.model.Gig;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GigServiceImplementation implements GigService{
+    @Override
+    public List<Gig> getAllGigs() {
+        return null;
+    }
+
+    @Override
+    public Gig getGigById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Gig saveGig(Gig gig) {
+        return null;
+    }
+
+    @Override
+    public Gig updateGig(Long id, Gig gig) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
 }
