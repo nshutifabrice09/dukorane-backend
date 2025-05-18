68ce0005e5e0534743ead34f284b8b5f24b3721f
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-18 12:43:06 +0200

    initial-service-impl-commit

--- src/main/java/com/dukorane/Dukorane_backend/service/SkillServiceImplementation.java
+++ src/main/java/com/dukorane/Dukorane_backend/service/SkillServiceImplementation.java
@@ -1,2 +1,34 @@
-package com.dukorane.Dukorane_backend.service;public class SkillServiceImplementation {
+package com.dukorane.Dukorane_backend.service;
+
+import com.dukorane.Dukorane_backend.model.Skill;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SkillServiceImplementation implements SkillService{
+    @Override
+    public List<Skill> getAllSkills() {
+        return null;
+    }
+
+    @Override
+    public Skill getSkillById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Skill saveSkill(Skill skill) {
+        return null;
+    }
+
+    @Override
+    public Skill updateSkill(Long id, Skill skill) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
 }
