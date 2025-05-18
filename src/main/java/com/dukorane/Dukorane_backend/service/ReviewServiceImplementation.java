1f1f0d95c9b5944f034e4660afa19c811557a3ae
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-18 12:42:36 +0200

    initial-service-impl-commit

--- src/main/java/com/dukorane/Dukorane_backend/service/ReviewServiceImplementation.java
+++ src/main/java/com/dukorane/Dukorane_backend/service/ReviewServiceImplementation.java
@@ -1,7 +1,34 @@
 package com.dukorane.Dukorane_backend.service;
 
+import com.dukorane.Dukorane_backend.model.Review;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ReviewServiceImplementation implements ReviewService{
+    @Override
+    public List<Review> getAllReviews() {
+        return null;
+    }
+
+    @Override
+    public Review getReviewById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Review saveReview(Review review) {
+        return null;
+    }
+
+    @Override
+    public Review updateReview(Long id, Review review) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
 }
