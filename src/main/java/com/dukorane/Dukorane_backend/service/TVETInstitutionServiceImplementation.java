0e0504b5b70e4ee79c420490f66fe8b830495afa
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-18 12:43:23 +0200

    initial-service-impl-commit

--- src/main/java/com/dukorane/Dukorane_backend/service/TVETInstitutionServiceImplementation.java
+++ src/main/java/com/dukorane/Dukorane_backend/service/TVETInstitutionServiceImplementation.java
@@ -1,2 +1,7 @@
-package com.dukorane.Dukorane_backend.service;public class TVETInstitutionServiceImplementation {
+package com.dukorane.Dukorane_backend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TVETInstitutionServiceImplementation implements TVETInstitutionService{
 }
