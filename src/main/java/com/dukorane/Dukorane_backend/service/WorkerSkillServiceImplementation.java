ec129b18eaea4fc9ed2e317185a433877784c7e3
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-18 12:48:40 +0200

    service-impl-commit

--- src/main/java/com/dukorane/Dukorane_backend/service/WorkerSkillServiceImplementation.java
+++ src/main/java/com/dukorane/Dukorane_backend/service/WorkerSkillServiceImplementation.java
@@ -1,2 +1,7 @@
-package com.dukorane.Dukorane_backend.service;public class WorkerSkillServiceImplementation {
+package com.dukorane.Dukorane_backend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WorkerSkillServiceImplementation implements WorkerProfileService{
 }
