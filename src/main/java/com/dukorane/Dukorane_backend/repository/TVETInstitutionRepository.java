f61183059490232728aeeb46c0a524a613f73978
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-18 11:21:15 +0200

    repository-updates-commit

--- src/main/java/com/dukorane/Dukorane_backend/repository/TVETInstitutionRepository.java
+++ src/main/java/com/dukorane/Dukorane_backend/repository/TVETInstitutionRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface TVETInstitutionRepository extends JpaRepository <TVETInstitution, UUID> {
+public interface TVETInstitutionRepository extends JpaRepository <TVETInstitution, Long> {
 }
