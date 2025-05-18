2f6c130fd2ef8d4eacf5fc0497c44a903f974ee9
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-18 12:43:51 +0200

    initial-service-impl-commit

--- src/main/java/com/dukorane/Dukorane_backend/service/TVETInstitutionServiceImplementation.java
+++ src/main/java/com/dukorane/Dukorane_backend/service/TVETInstitutionServiceImplementation.java
@@ -1,7 +1,34 @@
 package com.dukorane.Dukorane_backend.service;
 
+import com.dukorane.Dukorane_backend.model.TVETInstitution;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TVETInstitutionServiceImplementation implements TVETInstitutionService{
+    @Override
+    public List<TVETInstitution> getAllTVETInstitutions() {
+        return null;
+    }
+
+    @Override
+    public TVETInstitution getTVETInstitutionById(Long id) {
+        return null;
+    }
+
+    @Override
+    public TVETInstitution saveTVETInstitution(TVETInstitution tvetInstitution) {
+        return null;
+    }
+
+    @Override
+    public TVETInstitution updateTVETInstitution(Long id, TVETInstitution tvetInstitution) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
 }
