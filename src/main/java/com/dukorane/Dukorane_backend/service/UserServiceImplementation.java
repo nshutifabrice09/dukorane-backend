44d7af2f0f1006d7fc833bd68ddb9614ea150de3
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-19 10:23:10 +0200

    controller

--- src/main/java/com/dukorane/Dukorane_backend/service/UserServiceImplementation.java
+++ src/main/java/com/dukorane/Dukorane_backend/service/UserServiceImplementation.java
@@ -1,34 +1,54 @@
 package com.dukorane.Dukorane_backend.service;
 
 import com.dukorane.Dukorane_backend.model.User;
+import com.dukorane.Dukorane_backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserServiceImplementation implements UserService{
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImplementation(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public List<User> getAllUsers() {
-        return null;
+        return userRepository.findAll();
     }
 
     @Override
     public User getUserById(Long id) {
-        return null;
+        return userRepository.findUserById(id);
     }
 
     @Override
     public User saveUser(User user) {
-        return null;
+        return userRepository.save(user);
     }
 
     @Override
     public User updateUser(Long id, User user) {
+        User existUser = userRepository.findUserById(id);
+        if(existUser != null){
+            existUser.setFullName(user.getFullName());
+            existUser.setPhoneNumber(user.getPhoneNumber());
+            existUser.setPassword(user.getPassword());
+            existUser.setRole(user.getRole());
+            existUser.setLocation(user.getLocation());
+            return userRepository.save(user);
+        }
+
         return null;
     }
 
     @Override
     public void deleteById(Long id) {
-
+        userRepository.deleteById(id);
     }
 }
