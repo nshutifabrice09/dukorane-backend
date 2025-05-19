78172c3d4319bd2db1f44f54454ea4fd77431942
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-19 16:50:49 +0200

    latest-controller-commit

--- src/main/java/com/dukorane/Dukorane_backend/controller/TVETInstitutionController.java
+++ src/main/java/com/dukorane/Dukorane_backend/controller/TVETInstitutionController.java
@@ -1,9 +1,12 @@
 package com.dukorane.Dukorane_backend.controller;
 
 
+import com.dukorane.Dukorane_backend.model.TVETInstitution;
 import com.dukorane.Dukorane_backend.service.TVETInstitutionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
@@ -17,5 +20,14 @@ public class TVETInstitutionController {
         this.tvetInstitutionService = tvetInstitutionService;
     }
 
-    
+    @PostMapping("/tvetInstitution/{userId}")
+    public TVETInstitution save(@RequestBody TVETInstitution tvetInstitution, @PathVariable ("userId") Long userId){
+        return tvetInstitutionService.saveTVETInstitution(tvetInstitution, userId);
+    }
+
+    @GetMapping("/tvetInstitutions")
+    public List<TVETInstitution> tvetInstitutionList(){
+        return tvetInstitutionService.getAllTVETInstitutions();
+    }
+
 }
