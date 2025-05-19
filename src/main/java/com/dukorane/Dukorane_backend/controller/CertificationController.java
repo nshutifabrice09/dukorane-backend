aeb6a3a079131e0b167850fdebbb91180d1ad85e
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-19 22:27:51 +0200

    controller-updates-commit

--- src/main/java/com/dukorane/Dukorane_backend/controller/CertificationController.java
+++ src/main/java/com/dukorane/Dukorane_backend/controller/CertificationController.java
@@ -1,16 +1,18 @@
 package com.dukorane.Dukorane_backend.controller;
 
 
-import com.dukorane.Dukorane_backend.model.Certification;
 import com.dukorane.Dukorane_backend.service.CertificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.UUID;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
 public class CertificationController {
 
+    private final CertificationService certificationService;
+
+    @Autowired
+    public CertificationController(CertificationService certificationService) {
+        this.certificationService = certificationService;
+    }
 }
