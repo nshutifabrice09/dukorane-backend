0954f7d0c13de673cc4073f47a923c6d88c48dc9
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-19 10:57:23 +0200

    latest-controller-commit

--- src/main/java/com/dukorane/Dukorane_backend/controller/GigController.java
+++ src/main/java/com/dukorane/Dukorane_backend/controller/GigController.java
@@ -3,13 +3,22 @@ package com.dukorane.Dukorane_backend.controller;
 import com.dukorane.Dukorane_backend.model.Gig;
 import com.dukorane.Dukorane_backend.service.GigService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.UUID;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
 public class GigController {
 
+    private final GigService gigService;
+
+    @Autowired
+    public GigController(GigService gigService) {
+        this.gigService = gigService;
+    }
+
+    @PostMapping("/gig/{userId}/{skillId}")
+    public Gig saveGig(@RequestBody Gig gig, @PathVariable ("id") Long userId, @PathVariable ("id") Long skillId){
+        return gigService.saveGig(gig, userId, skillId);
+    }
 }
