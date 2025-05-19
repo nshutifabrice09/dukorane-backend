e2352b3e2275fd8ce1ee422aa552358acb4664b6
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-19 11:05:06 +0200

    latest-controller-commit

--- src/main/java/com/dukorane/Dukorane_backend/service/SkillServiceImplementation.java
+++ src/main/java/com/dukorane/Dukorane_backend/service/SkillServiceImplementation.java
@@ -1,29 +1,44 @@
 package com.dukorane.Dukorane_backend.service;
 
 import com.dukorane.Dukorane_backend.model.Skill;
+import com.dukorane.Dukorane_backend.repository.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class SkillServiceImplementation implements SkillService{
+    private final SkillRepository skillRepository;
+
+    @Autowired
+    public SkillServiceImplementation(SkillRepository skillRepository) {
+        this.skillRepository = skillRepository;
+    }
+
     @Override
     public List<Skill> getAllSkills() {
-        return null;
+        return skillRepository.findAll();
     }
 
     @Override
     public Skill getSkillById(Long id) {
-        return null;
+        return skillRepository.findSkillById(id);
     }
 
     @Override
     public Skill saveSkill(Skill skill) {
-        return null;
+        return skillRepository.save(skill);
     }
 
     @Override
     public Skill updateSkill(Long id, Skill skill) {
+        Skill existSkill = skillRepository.findSkillById(id);
+        if(existSkill != null){
+            existSkill.setName(skill.getName());
+            existSkill.setCategory(skill.getCategory());
+            return skillRepository.save(existSkill);
+        }
         return null;
     }
 
