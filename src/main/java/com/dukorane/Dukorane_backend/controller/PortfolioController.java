30fc38462a208ac2d0fc49d2acf919b5d1609b2b
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-20 13:10:41 +0200

    controller-commit

--- src/main/java/com/dukorane/Dukorane_backend/controller/PortfolioController.java
+++ src/main/java/com/dukorane/Dukorane_backend/controller/PortfolioController.java
@@ -1,8 +1,11 @@
 package com.dukorane.Dukorane_backend.controller;
 
+import com.dukorane.Dukorane_backend.model.Portfolio;
 import com.dukorane.Dukorane_backend.service.PortfolioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
@@ -14,5 +17,14 @@ public class PortfolioController {
     public PortfolioController(PortfolioService portfolioService) {
         this.portfolioService = portfolioService;
     }
-    
+
+    @PostMapping("/portfolio/{workerId}")
+    public Portfolio save(@RequestBody Portfolio portfolio, @PathVariable ("workerId") Long workerId){
+        return portfolioService.savePortfolio(portfolio, workerId);
+    }
+
+    @GetMapping("/portfolios")
+    public List<Portfolio> portfolioList(){
+        return portfolioService.getAllPortfolios();
+    }
 }
