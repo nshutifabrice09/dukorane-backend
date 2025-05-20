61eb735f1a5e2d9ac3357c176e3bf2763b32edc3
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-20 13:32:15 +0200

    controller-updates-commit

--- src/main/java/com/dukorane/Dukorane_backend/controller/WorkerProfileController.java
+++ src/main/java/com/dukorane/Dukorane_backend/controller/WorkerProfileController.java
@@ -1,19 +1,19 @@
 package com.dukorane.Dukorane_backend.controller;
 
-
-import com.dukorane.Dukorane_backend.model.WorkerProfile;
 import com.dukorane.Dukorane_backend.service.WorkerProfileService;
-import org.hibernate.jdbc.Work;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.UUID;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
 public class WorkerProfileController {
 
+    private final WorkerProfileService workerProfileService;
 
 
+    @Autowired
+    public WorkerProfileController(WorkerProfileService workerProfileService) {
+        this.workerProfileService = workerProfileService;
+    }
+    
 }
