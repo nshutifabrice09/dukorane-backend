8d7d1ea4a8534b132d22706ff6d7470d95462258
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-20 13:08:59 +0200

    controller-commit

--- src/main/java/com/dukorane/Dukorane_backend/controller/PortfolioController.java
+++ src/main/java/com/dukorane/Dukorane_backend/controller/PortfolioController.java
@@ -1,16 +1,18 @@
 package com.dukorane.Dukorane_backend.controller;
 
-import com.dukorane.Dukorane_backend.model.Portfolio;
 import com.dukorane.Dukorane_backend.service.PortfolioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import javax.sound.sampled.Port;
-import java.util.List;
-import java.util.UUID;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
 public class PortfolioController {
 
+    private final PortfolioService portfolioService;
+
+    @Autowired
+    public PortfolioController(PortfolioService portfolioService) {
+        this.portfolioService = portfolioService;
+    }
+    
 }
